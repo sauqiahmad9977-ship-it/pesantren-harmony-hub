0546db0f1b827c42633b596c697848e3c1419cc4
Firnando Smauna <firnandosmauna@gmail.com>
2026-08-05 07:10:08 +0700

    Hapus vercel.json agar Nitro bekerja normal

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,6 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
-  nitro: {
-    preset: "vercel"
-  },
   optimizeDeps: {
     include: [
       "lucide-react", 
